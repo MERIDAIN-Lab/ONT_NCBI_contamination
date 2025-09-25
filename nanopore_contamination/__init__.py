e8ef4f9f2482f1f1614d83b2939e43d84ce10b0e
Flor Martino <45692378+florenmartino@users.noreply.github.com>
2025-09-24 22:00:02 -0400

    Delete nanopore_contamination directory

--- nanopore_contamination/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Nanopore adapter contamination analysis package."""
-
-from .cli import main
-from .pipeline import PipelineConfig, run_pipeline
-
-__all__ = ["main", "PipelineConfig", "run_pipeline"]
